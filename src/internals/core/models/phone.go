89970b2f16e236df4b97b17d4ed6710e7190e102
gcarreno <german.carr@gmail.com>
2024-11-06 15:17:33 +0100

    removed http server developer with hexagonal architecture

--- src/internals/core/models/phone.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Phone struct {
-	CodeCountry string `json:"code_country"`
-	CodeArea    string `json:"code_area"`
-	Number      string `json:"number"`
-}
--- src/internals/core/models/place.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Place struct {
-	ID       int64   `json:"id"`
-	Name     string  `json:"name"`
-	Category string  `json:"category"`
-	Address  Address `json:"address"`
-}
